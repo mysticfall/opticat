02bb9cbe6bcec2360cd44a58eb7f28044d89c1df
Xavier Cho <mysticfallband@gmail.com>
2024-01-07 17:23:43 +0900

    Refactor game API and add relevant tests
    Updated game.ts file, refactoring the game logic and improving the game data binder type. This allows more flexible binding and updating of game world state and game actors. A new test file, game.test.ts was also added to ensure the new game logic works as expected.

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,7 +1,9 @@
+import * as E from "fp-ts/Either"
 import {Either} from "fp-ts/Either"
+import {pipe} from "fp-ts/function"
 import * as T from "io-ts"
 import {MaxLengthString, MinLengthString} from "../common"
-import {ActorData, ActorHolder} from "./actor"
+import {Actor, ActorData, ActorHolder} from "./actor"
 import {WorldData} from "./world"
 
 export const GameTitle = T.intersection([
@@ -29,15 +31,89 @@ export const GameInfo = T.readonly(T.intersection([
 
 export type GameInfo = T.TypeOf<typeof GameInfo>
 
-export interface Game<
-    TActor extends ActorData = unknown & ActorData,
-    TWorld extends WorldData<TActor> = unknown & WorldData<TActor>
-> {
-    readonly info: GameInfo
+export type GameDataBinder<TWorld> = {
 
-    readonly actors: ActorHolder<TActor>
+    readonly Do: Either<never, { context: TWorld }>
+
+    bind: <N extends string, A extends { context: TWorld }, E2, B>(
+        name: Exclude<N, keyof A>,
+        f: (a: A) => (context: TWorld) => Either<E2, B>
+    ) => <E1>(ma: Either<E1, A>) => Either<E1 | E2, { readonly [K in N | keyof A]: K extends keyof A ? A[K] : B }>
+
+    update: <A extends { context: TWorld }, E2>(
+        f: (a: A) => (context: TWorld) => Either<E2, TWorld>
+    ) => <E1>(ma: Either<E1, A>) => Either<E1 | E2, A>
+}
+
+export interface Game<
+    TInfo extends GameInfo = unknown & GameInfo,
+    TActorData extends ActorData = unknown & ActorData,
+    TWorld extends WorldData<TActorData> = unknown & WorldData<TActorData>,
+    TActor extends Actor<TActorData, TWorld> = unknown & Actor<TActorData, TWorld>
+> {
+    readonly info: TInfo
+
+    readonly actors: ActorHolder<TActorData, TWorld, TActor>
 
     get world(): TWorld
 
-    update(updater: (context: TWorld) => Either<Error, TWorld>): Either<Error, void>
+    update<T extends { context: TWorld }, E>(
+        f: (g: GameDataBinder<TWorld>) => Either<E, T>
+    ): Either<E, T>
+}
+
+export class BaseGame<
+    TInfo extends GameInfo = unknown & GameInfo,
+    TActorData extends ActorData = unknown & ActorData,
+    TWorld extends WorldData<TActorData> = unknown & WorldData<TActorData>,
+    TActor extends Actor<TActorData, TWorld> = unknown & Actor<TActorData, TWorld>
+> implements Game<TInfo, TActorData, TWorld, TActor> {
+
+    private _world: TWorld
+
+    constructor(
+        readonly info: TInfo,
+        readonly actors: ActorHolder<TActorData, TWorld, TActor>,
+        initialWorld: TWorld) {
+
+        this._world = initialWorld
+    }
+
+    get world(): TWorld {
+        return this._world
+    }
+
+    private readonly _bind: GameDataBinder<TWorld>["bind"] = (name, f) => ma => pipe(
+        ma,
+        E.chain(a => pipe(
+            ma,
+            E.map(f),
+            E.chainW(f2 => f2(a.context)),
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            E.map(v => Object.assign({}, a, {[name]: v}) as any)
+        ))
+    )
+
+    private readonly _update: GameDataBinder<TWorld>["update"] = f => ma => pipe(
+        pipe(E.of(f), E.ap(ma)),
+        E.ap(pipe(ma, E.map(({context}) => context))),
+        E.flattenW,
+        E.chainW(context => pipe(ma, E.map(a => ({...a, context: context}))))
+    )
+
+    update<T extends { context: TWorld }, E>(f: (
+        g: GameDataBinder<TWorld>) => Either<E, T>
+    ): Either<E, T> {
+        const result = f({
+            Do: E.of({context: this.world}),
+            bind: this._bind,
+            update: this._update
+        })
+
+        if (E.isRight(result)) {
+            this._world = result.right.context
+        }
+
+        return result
+    }
 }
